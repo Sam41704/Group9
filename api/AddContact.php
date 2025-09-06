b4954f8133ddc9b631781ca0002eaaf82a68a424
8kJordan <thepro321leyskil@gmail.com>
2025-09-06 18:30:42 -0400

    Add AddContact API Endpoint
    Change: Add AddContat API endpoint, with error handling and common duplication pitfalls accounted for
    
    Why?: Necessary for basic operations in contacts app
    
    Authored By: Jordan Sanchez

--- api/AddContact.php
+++ api/AddContact.php
@@ -62,16 +62,93 @@ try{
     exit();
 }
 
+# if the contact we want to create already exists for the given user, return 400 error
+if (contactExists($db, $payload)) {
+    http_response_code(400);
+
+    echo json_encode([
+        "status" => "error",
+        "contactCreated" => false,
+        "errType" => "ContactExistsError",
+        "desc" => "This contact already exists"
+    ]);
+    exit();
+}
+
+$query = $db->prepare("INSERT INTO contacts (FirstName, LastName, Phone, Email, UserId) VALUES (?,?,?,?,?)");
+$query->bind_param("ssssi",
+    $payload["firstName"],
+    $payload["lastName"],
+    $payload["phone"],
+    $payload["email"],
+    $payload["userId"]);
+
+# performing insert contact operation
+try{
+    $query->execute();
+
+    http_response_code(200);
+
+    echo json_encode([
+        "status" => "success",
+        "contactCreated" => true,
+    ]);
+} catch (Exception $e){
+    $errMessage = $e->getMessage();
+    $errCode = $e->getCode();
+
+    if ($errCode == 1452){ # foreign key violation, attempted to bind contact to non-existent userId
+        http_response_code(400);
+
+        echo json_encode([
+            "status" => "error",
+            "contactCreated" => false,
+            "errType" => "NonexistentUserIDError",
+            "desc" => "The specified user ID does not exist"
+        ]);
+    } elseif ($errCode == 1062){ # duplicate unique entry error TODO this would be the conditional that checks for duplicate phone or email from a DB err
+        $userId = $payload["userId"];
+        http_response_code(400);
+
+        echo json_encode([
+            "status" => "error",
+            "contactCreated" => false,
+            "errType" => "ContactExistsError",
+            "desc" => "The specified contact already exists for user $userId"
+        ]);
+    } else{
+        http_response_code(500);
+
+        echo json_encode([
+            "status" => "error",
+            "contactCreated" => false,
+            "errType" => "ContactCreationError",
+            "desc" => "Failed to to create contact"
+        ]);
+    }
+} finally {
+    $db->close();
+    $query->close();
+}
 
 function contactExists(mysqli $conn, array $payload): bool{
     # gathers count of rows of contacts that meet the given conditions
-    $query = $conn->prepare("SELECT COUNT(*) FROM Contacts WHERE (Email = ? AND UserId = ?)");
-    $query->bind_param("si", $payload["email"], $payload["userId"]); // TODO need to check if this is proper way to describe int
+    $query = $conn->prepare("SELECT COUNT(*) AS cnt FROM Contacts WHERE ((Email = ? OR Phone = ?)AND UserId = ?)");
+    $query->bind_param("ssi", $payload["email"], $payload["phone"], $payload["userId"]);
 
-    $query->execute(); // TODO cont here
-    return true;
+    $query->execute();
+    $result = $query->get_result();
+
+    $returnValue = true;
+    if($result->fetch_assoc()["cnt"] === 0)
+        $returnValue = false;
+
+    $query->close();
+    return $returnValue;
 }
 
-function getRequestPayload(): array{
+function getRequestPayload(): array|null{
     return json_decode(file_get_contents("php://input"), true);
 }
+
+# TODO what we need to do is have one table that enforces per user email and phone number uniqueness, don't know if we really want this or not
