e770f222eb63ac38cd85fbf2937683c4ce14a02c
isabellabuitano <isabuitano@icloud.com>
2025-09-07 19:35:16 -0400

    Continuing to debug auth/reg

--- api/Auth.php
+++ api/Auth.php
@@ -56,14 +56,13 @@ try{
     }
 
 } catch (Throwable $e){
-    error_log("DB connect failed: " . $e->getMessage());
     http_response_code(500);
     echo json_encode([
         "status" => "error",
         "errType" => "ServerError",
         //temporarily commenting out the error desc
       //  "desc" => "Failed to make DB connection"
-      "desc"  => $e->getMessage();
+      "desc"  => $e->getMessage()
     ]);
     exit();
 }
